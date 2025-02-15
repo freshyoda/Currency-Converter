3dc4e9a2c422f75c93403c921dc497f4768d4e37
freshyoda <tequila01.007@gmail.com>
2025-02-15 18:18:56 +0530

    updates03

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//file:///C:/Users/Agamjot%20Singh/Downloads/sem(4)/CODE/js/API/currencyConv/index.html
+//file:///C:/Users/Agamjot%20Singh/Downloads/sem(4)/CODE/js/API/Currency-Converter/index.html
 //https://www.youtube.com/watch?v=CyGodpqcid4&list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW&index=15
 const countryList = {
     AED: "AE",
